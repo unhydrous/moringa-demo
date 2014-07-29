b4f5b99012fec14d769e29a16a9238c3cbcd7206
africastalking <gikandi@gmail.com>
2014-07-29 20:53:33 +0300

    - Remove extraneous logging in dlr processor

--- dlrProcessor.py
+++ dlrProcessor.py
@@ -14,8 +14,7 @@ class DlrEntry:
 class DlrProcessor:
 
     def __init__(self):
-        log.msg("DlrProcessor is starting...")
-	self.queue   = defer.DeferredQueue()
+        self.queue   = defer.DeferredQueue()
 	self.workers = [self.worker() for _ in range(10)]
 	
 	self.safaricomDlrUrl = Config['safaricomDlrUrl']
@@ -23,8 +22,7 @@ class DlrProcessor:
 	self.routeSmsDlrUrl  = Config['routeSmsDlrUrl']
 
     def addSafaricomEntry(self, phoneNumber, transactionId):
-	log.msg("Adding safaricom entry with phoneNumber %s and transactionId %s" % (phoneNumber, transactionId))
-        self.queue.put(DlrEntry('safaricom', '%s:%s' % (phoneNumber, transactionId)))
+	self.queue.put(DlrEntry('safaricom', '%s:%s' % (phoneNumber, transactionId)))
 
     def addKenyaEntry(self, phoneNumber, transactionId):
 	self.queue.put(DlrEntry('kenya', '%s:%s' % (phoneNumber, transactionId)))
@@ -34,27 +32,21 @@ class DlrProcessor:
 	
     @defer.inlineCallbacks
     def worker(self):
-        log.msg("SNG: Queue::worker has been called")
-	while 1:
-            log.msg("SNG: Queue is running. The length is %s" % self.queue.size)
-	    entry = yield self.queue.get()
+        while 1:
+            entry = yield self.queue.get()
             try:
                 yield self._sendReport(entry)
             except Exception, e:
-                log.msg("SNG: There was an error and it is %s" % str(e))
-            else:
-                log.msg("Great success")
+                log.msg("ERROR_ALERT while sending report: %s" % str(e))
 
     @defer.inlineCallbacks
     def _sendReport(self, entry):
-	log.msg("SNG: We are sending the entry with gateway %s and token %s" % (entry.gateway, entry.token))
-        if entry.gateway == 'safaricom':
+	if entry.gateway == 'safaricom':
 	    phoneNumber, messageId = entry.token.split(':')
 	    data = {'messageId'   : messageId,
 		    'phoneNumber' : phoneNumber,
 		    'status'      : 1}
-	    log.msg("OK, this is safaricom. Lets send it")
-            response = yield self._sendToApi(self.safaricomDlrUrl, data)
+	    response = yield self._sendToApi(self.safaricomDlrUrl, data)
 	    log.msg("Send to safaricom has received response " + response)
 
 	elif entry.gateway == 'kenya':
@@ -63,31 +55,27 @@ class DlrProcessor:
 	    data = {'messageId'   : messageId,
 		    'phoneNumber' : phoneNumber,
 		    'status'      : 1}
-            log.msg("OK, this is kenya. Lets send it")
-	    response = yield self._sendToApi(self.kenyaDlrUrl, data)
+            response = yield self._sendToApi(self.kenyaDlrUrl, data)
 	    log.msg("Send so kenya has received response " + response)
 
 	elif entry.gateway == 'routeSms':
 	    data = {'messageId' : entry.token,
 		    'status'    : 'DELIVRD'}
-            log.msg("OK, this is routeSms. Lets send it")
-	    response = yield self._sendToApi(self.routeSmsDlrUrl, data)
+            response = yield self._sendToApi(self.routeSmsDlrUrl, data)
 	    log.msg("Send to routeSms has received response " + response)
 
         else:
-            log.msg("This is an unknown gateway: " + entry.gateway)
+            log.msg("ERROR_ALERT Unknown gateway: " + entry.gateway)
 
     @defer.inlineCallbacks
     def _sendToApi(self, url, data):
-        log.msg("Send to api called with url=%s" % url)
         data = urllib.urlencode(data)
         try:
             response = yield client.getPage(url, method="POST", postdata=data, headers={'Content-Type':'application/x-www-form-urlencoded'})
         except Exception, e:
             # TODO: testcase for this
-            log.msg("URL call has failed with error %s" % str(e))
+            log.msg("ERROR_ALERT URL call has failed with error %s" % str(e))
             response = 'URL Call Failed with error %s' % str(e)
             defer.returnValue(response)
         else:
-            log.msg("URL call has succeeded with  %s" % response.strip())
             defer.returnValue(response.strip())
